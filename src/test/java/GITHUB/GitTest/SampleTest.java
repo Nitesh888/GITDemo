f4d3566f5a16076d96c7c887702e82670ba99f6c
Nitesh888 <m.nani.yuvi@gmail.com>
2024-04-29 03:59:38 +0530

    first commit

---
+++ src/test/java/GITHUB/GitTest/SampleTest.java
@@ -0,0 +1,13 @@
+package GITHUB.GitTest;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	  System.out.println("Hello");
+	  System.out.println("goodBye");
+		
+	}
+
+}
